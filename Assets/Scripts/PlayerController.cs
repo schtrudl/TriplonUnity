55623234edf631a905af4026fb4ebdd5e24f1373
DijonShala <dijon.shala7@gmail.com>
2025-01-01 23:35:43 +0100

    player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10;
+    public float speed = 20f;
+    public float turnSpeed = 100f;
+    public float tiltAngle = 30f;
+    public float tiltSpeed = 2f;
 
+    private float currentTilt = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +17,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, -Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
+        Vector3 forwardMovement = transform.right * verticalInput * speed * Time.deltaTime;
+        transform.position += forwardMovement;
+
+        float rotation = horizontalInput * turnSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        float targetTilt = -horizontalInput * tiltAngle;
+        currentTilt = Mathf.Lerp(currentTilt, targetTilt, Time.deltaTime * tiltSpeed);
+
+        Quaternion targetRotation = Quaternion.Euler(currentTilt, transform.eulerAngles.y, transform.eulerAngles.z);
+        transform.rotation = targetRotation;
     }
 }
