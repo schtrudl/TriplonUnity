340fd8dee8ea670250b146f2c6d7e271d3c51cb4
DijonShala <dijon.shala7@gmail.com>
2025-01-03 02:47:04 +0100

    add physics to movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
+    public float maxSpeed = 20f;
+    public float acceleration = 5f;
+    public float deceleration = 10f;
     public float turnSpeed = 100f;
     public float tiltAngle = 30f;
     public float tiltSpeed = 2f;
 
+    private float currentSpeed = 0f;
+    private float targetSpeed = 0f;
     private float currentTilt = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float centreOfGravityOffset = -1f;
+    private Rigidbody rigidBody;
+
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.centerOfMass += Vector3.up * centreOfGravityOffset;
 
+        // Enable interpolation for smoother physics
+        rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float verticalInput = 0f;
@@ -25,17 +34,30 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey("a")) horizontalInput = -1f;
         if (Input.GetKey("d")) horizontalInput = 1f;
 
-        Vector3 forwardMovement = transform.right * verticalInput * speed * Time.deltaTime;
-        transform.position += forwardMovement;
+        // Acceleration and deceleration
+        if (verticalInput != 0)
+        {
+            targetSpeed = verticalInput * maxSpeed;
+            currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * acceleration);
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.deltaTime);
+        }
 
+        // Forward movement
+        Vector3 forwardMovement = transform.right * currentSpeed;
+        Vector3 velocity = new Vector3(forwardMovement.x, rigidBody.linearVelocity.y, forwardMovement.z);
+        rigidBody.linearVelocity = velocity;
+
+        // Turning
         float rotation = horizontalInput * turnSpeed * Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
+        Quaternion deltaRotation = Quaternion.Euler(0f, rotation, 0f);
+        rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
 
         float targetTilt = -horizontalInput * tiltAngle;
-
         currentTilt = Mathf.Lerp(currentTilt, targetTilt, Time.deltaTime * tiltSpeed);
 
-        Quaternion targetRotation = Quaternion.Euler(currentTilt, transform.eulerAngles.y, transform.eulerAngles.z);
-        transform.rotation = targetRotation;
+
     }
 }
