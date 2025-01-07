7d7b0e400806776956ad0f730b5f843ee385e6eb
BunnyBoy66 <gluvic.samo@gmail.com>
2025-01-07 01:21:16 +0100

    prosojnost in popravljena orientacija trikotnikov

--- Assets/Scripts/Trail1.cs
+++ Assets/Scripts/Trail1.cs
@@ -32,6 +32,7 @@ public class Trail1 : MonoBehaviour
     private GameObject _previusSegment;
     private Vector3 _previusColliderCenter;
     private Vector3 _previusColliderSize;
+    private Material _material;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -50,21 +51,49 @@ public class Trail1 : MonoBehaviour
 
         // Define triangles (counter-clockwise order for each face)
         _triangles = new int[36];
-        _triangles[0] = 0; _triangles[1] = 1; _triangles[2] = 5;
-        _triangles[3] = 0; _triangles[4] = 5; _triangles[5] = 4;
-        _triangles[6] = 2; _triangles[7] = 6; _triangles[8] = 7;
-        _triangles[9] = 2; _triangles[10] = 7; _triangles[11] = 3;
-        _triangles[15] = 0; _triangles[16] = 6; _triangles[17] = 2;
-        _triangles[18] = 1; _triangles[19] = 3; _triangles[20] = 7;
-        _triangles[21] = 1; _triangles[22] = 7; _triangles[23] = 5;
-        _triangles[24] = 4; _triangles[25] = 5; _triangles[26] = 7;
-        _triangles[27] = 4; _triangles[28] = 7; _triangles[29] = 6;
-        _triangles[12] = 0; _triangles[13] = 4; _triangles[14] = 6;
-        _triangles[30] = 0; _triangles[31] = 2; _triangles[32] = 3;
-        _triangles[33] = 0; _triangles[34] = 3; _triangles[35] = 1;
-    }
+        // Front Face (Closer to Camera)
+        _triangles[0] = 2; _triangles[1] = 0; _triangles[2] = 1; // Triangle 1
+        _triangles[3] = 1; _triangles[4] = 3; _triangles[5] = 2; // Triangle 2
 
-    // Update is called once per frame
+        // Back Face (Further from Camera)
+        _triangles[6] = 7; _triangles[7] = 5; _triangles[8] = 4; // Triangle 1
+        _triangles[9] = 4; _triangles[10] = 6; _triangles[11] = 7; // Triangle 2
+
+        // Left Side (Connect Front and Back on Left)
+        _triangles[12] = 6; _triangles[13] = 4; _triangles[14] = 0; // Triangle 1
+        _triangles[15] = 0; _triangles[16] = 2; _triangles[17] = 6; // Triangle 2
+
+        // Right Side (Connect Front and Back on Right)
+        _triangles[18] = 3; _triangles[19] = 1; _triangles[20] = 5; // Triangle 1
+        _triangles[21] = 5; _triangles[22] = 7; _triangles[23] = 3; // Triangle 2
+
+        // Top Side (Connect Front and Back on Top)
+        _triangles[24] = 0; _triangles[25] = 4; _triangles[26] = 5; // Triangle 1
+        _triangles[27] = 5; _triangles[28] = 1; _triangles[29] = 0; // Triangle 2
+
+        // Bottom Side (Connect Front and Back on Bottom)
+        _triangles[30] = 6; _triangles[31] = 2; _triangles[32] = 3; // Triangle 1
+        _triangles[33] = 3; _triangles[34] = 7; _triangles[35] = 6; // Triangle 2
+
+        _material = _cycle.GetComponentInChildren<MeshRenderer>().materials[0];
+        _material.SetFloat("_Surface", 1);
+        // Set the render mode to Transparent
+        _material.SetFloat("_Mode", 3); // 3 is for Transparent mode in Unity's standard shader
+        _material.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Back);
+        _material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        _material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        _material.SetInt("_ZWrite", 0);  // Disable ZWrite for transparency
+        _material.DisableKeyword("_ALPHATEST_ON");
+        _material.EnableKeyword("_ALPHABLEND_ON");
+        _material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        _material.renderQueue = 3000; // Set render queue to be in the transparent range
+
+        // Adjust the transparency (alpha value) of the material
+        Color materialColor = _material.color;  // Get the material's color
+        materialColor.a = 0.5f;  // Set alpha to 50% transparency
+        _material.color = materialColor;
+    }
+        // Update is called once per frame
     void LateUpdate()
     {
         if (Vector3.Distance(_upL.transform.position, _previusUpL) <= _dist || 
@@ -76,12 +105,12 @@ public class Trail1 : MonoBehaviour
         _vertices = new Vector3[8];
 
         _vertices[0] = _previusUpL;
-        _vertices[1] = _previusDownL;
-        _vertices[2] = _previusUpR;
+        _vertices[1] = _previusUpR;
+        _vertices[2] = _previusDownL;
         _vertices[3] = _previusDownR;
         _vertices[4] = _upL.transform.position;
-        _vertices[5] = _downL.transform.position;
-        _vertices[6] = _upR.transform.position;
+        _vertices[5] = _upR.transform.position;
+        _vertices[6] = _downL.transform.position;
         _vertices[7] = _downR.transform.position;
 
         // Create a new child object under _trailMesh
@@ -104,7 +133,7 @@ public class Trail1 : MonoBehaviour
         meshFilter.mesh = segmentMesh;
 
         // Optional: Assign a material to the MeshRenderer
-        meshRenderer.material = _cycle.GetComponentInChildren<MeshRenderer>().materials[0]; // Replace with your desired material
+        meshRenderer.material = _material;
 
         if (_previusSegment)
         {
