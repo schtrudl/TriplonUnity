d2a0c0d33328f2935b0928e3932c0f86c80dd139
Dijon <82019954+DijonShala@users.noreply.github.com>
2025-01-07 13:45:14 +0100

    Player control (#3)
    * player controller
    
    * demo controller
    
    * add physics to movement
    
    * add tilt and smother movement
    
    * some fixes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10;
+    public float maxSpeed = 25f;
+    public float acceleration = 0.2f;
+    public float deceleration = 3f;
+    public float turnSpeed = 70f;
+    public float tiltAngle = 20f;
+    public float tiltSpeed = 1.5f;
+    public float maxRotationSpeed = 70f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float currentSpeed = 0f;
+    private float targetSpeed = 0f;
+    private float currentTilt = 0f;
+    private float defaultTilt = 0f;
+    public float centreOfGravityOffset = -1f;
+    private Rigidbody rigidBody;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.centerOfMass += Vector3.up * centreOfGravityOffset;
+
+        // enable interpolation for smoother physics
+        rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, -Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        float verticalInput = 0f;
+        float horizontalInput = 0f;
+
+        if (Input.GetKey("w")) verticalInput = 1f;
+        if (Input.GetKey("s")) verticalInput = -1f;
+        if (Input.GetKey("a")) horizontalInput = -1f;
+        if (Input.GetKey("d")) horizontalInput = 1f;
+
+        // acceleration and deceleration
+        if (verticalInput != 0)
+        {
+            targetSpeed = verticalInput * maxSpeed;
+            currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * acceleration);
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.deltaTime);
+        }
+
+        // forward movement
+        Vector3 forwardMovement = transform.right * currentSpeed;
+        Vector3 velocity = new Vector3(forwardMovement.x, rigidBody.linearVelocity.y, forwardMovement.z);
+        rigidBody.linearVelocity = velocity;
+
+        // set turning speed based on current speed
+        float dynamicTurnSpeed = turnSpeed * (currentSpeed / maxSpeed);
+        dynamicTurnSpeed = Mathf.Clamp(dynamicTurnSpeed, -maxRotationSpeed, maxRotationSpeed);
+
+        if (currentSpeed > 0.1f || currentSpeed < 0.1f)
+        {
+            // turn
+            float rotation = horizontalInput * dynamicTurnSpeed * Time.deltaTime;
+            Quaternion deltaRotation = Quaternion.Euler(0f, rotation, 0f);
+            rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+
+            // tilt
+            float targetTilt = -horizontalInput * tiltAngle;
+            currentTilt = Mathf.Lerp(currentTilt, targetTilt, Time.deltaTime * tiltSpeed);
+        }
+        else
+        {
+            // reset tilt when stopped
+            currentTilt = Mathf.Lerp(currentTilt, defaultTilt, Time.deltaTime * tiltSpeed);
+        }
+
+        Quaternion tiltRotation = Quaternion.Euler(currentTilt, rigidBody.rotation.eulerAngles.y, rigidBody.rotation.eulerAngles.z);
+        rigidBody.MoveRotation(tiltRotation);
 
     }
 }
